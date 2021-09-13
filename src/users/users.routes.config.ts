ef0818c1f02442ab10347eae8fc012990bc26f78
Mokrov Vladimir <vovkamokroff@gmail.com>
2021-09-13 14:14:08 +0300

    init with ts

---
+++ src/users/users.routes.config.ts
@@ -0,0 +1,39 @@
+import express = require('express');
+import CommonRoutesConfig from '../common/common.routes.config';
+
+class UsersRoutes extends CommonRoutesConfig {
+
+  constructor(app: express.Application) {
+    super(app, 'UserRoutes');
+  }
+
+  configureRoutes(): express.Application {
+    this.app.route(`/users`)
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send('List of users');
+      })
+      .post((req:express.Request, res:express.Response) => {
+        res.status(200).send('Post users');
+      });
+
+    this.app.route(`/users/:userId`)
+      .all((req: express.Request, res: express.Response, next: express.NextFunction) => {
+        next();
+      })
+      .get((req: express.Request, res: express.Response) => {
+        res.status(200).send(`GET requested for id ${req.params.userId}`);
+      })
+      .put((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PUT requested for id ${req.params.userId}`);
+      })
+      .patch((req: express.Request, res: express.Response) => {
+        res.status(200).send(`PATCH requested for id ${req.params.userId}`);
+      })
+      .delete((req: express.Request, res: express.Response) => {
+        res.status(200).send(`DELETE requested for id ${req.params.userId}`);
+      });
+    return this.app;
+  }
+}
+
+export default UsersRoutes;
