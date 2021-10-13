83b963a11c84e187677c0560652a19e5770bd36b
just_vovka <vovkamokroff@gmail.com>
2021-10-14 00:14:18 +0300

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,10 @@ import {debugLog, loggerOptions} from "./middleware/log";
 
 const app:express.Application = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 app.use(expressWinston.logger(loggerOptions));
 
